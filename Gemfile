bc4ab4c1fa1dfd8949887eb67bf250bd5153463e
Frédéric de Villamil <frederic@de-villamil.com>
2010-07-27 23:01:36 +0200

    Updating the configuration files

---
+++ Gemfile
@@ -0,0 +1,16 @@
+# Edit this Gemfile to bundle your application's dependencies.
+# This preamble is the current preamble for Rails 3 apps; edit as needed.
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.rc'
+
+gem 'htmlentities'
+gem 'json'
+gem 'calendar_date_select'
+gem 'bluecloth', '~> 2.0.5'
+gem 'coderay', '~> 0.8'
+gem 'will_paginate', '~> 3.0.pre2'
+gem 'RedCloth', '~> 4.2.2'
+# gem 'fdv-actionwebservice', '2.3.8', :require => 'actionwebservice'
+gem 'addressable', '~> 2.1.0', :require => 'addressable/uri'
+gem 'mini_magick', '~> 1.2.5', :require => 'mini_magick'
---
+++ config/application.rb
@@ -0,0 +1,87 @@
+require File.expand_path('../boot', __FILE__)
+
+module Typo
+  class Application < Rails::Application
+    # Skip frameworks you're not going to use
+    config.frameworks -= [ :active_resource ]
+  
+    # Setup the cache path
+    config.action_controller.page_cache_directory = "#{RAILS_ROOT}/public/cache/"
+    config.cache_store=:file_store, "#{RAILS_ROOT}/public/cache/"
+  
+  
+    # I need the localization plugin to load first
+    # Otherwise, I can't localize plugins <= localization
+    # Forcing manually the load of the textfilters plugins fixes the bugs with apache in production.
+    config.plugins = [ :localization, :all ]
+    
+    config.load_paths += %W(
+      vendor/rubypants
+      vendor/akismet
+      vendor/flickr
+      vendor/uuidtools/lib
+      vendor/rails/railties
+      vendor/rails/railties/lib
+      vendor/rails/actionpack/lib
+      vendor/rails/activesupport/lib
+      vendor/rails/activerecord/lib
+      vendor/rails/actionmailer/lib
+      app/apis
+    ).map {|dir| "#{RAILS_ROOT}/#{dir}"}.select { |dir| File.directory?(dir) }
+    
+    # Use the filesystem for sessions instead of the database
+    config.action_controller.session = { :key => "_typo_session", :secret => "8d7879bd56b9470b659cdcae88792622" }
+    
+    # Disable use of the Accept header, since it causes bad results with our
+    # static caching (e.g., caching an atom feed as index.html).
+    config.action_controller.use_accept_header = false
+  
+    # Activate observers that should always be running
+    # config.active_record.observers = :cacher, :garbage_collector
+    config.active_record.observers = :email_notifier, :web_notifier
+  end
+  
+  # Load included libraries.
+  require 'rubypants'
+  require 'uuidtools'
+  
+  require 'migrator'
+  require 'rails_patch/active_record'
+  require 'rails_patch/active_support'
+  require 'login_system'
+  require 'typo_version'
+  $KCODE = 'u'
+  require 'jcode'
+  require 'transforms'
+  
+  $FM_OVERWRITE = true
+  require 'filemanager'
+  
+  ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
+    :long_weekday => '%a %B %e, %Y %H:%M'
+  )
+  
+  ActionMailer::Base.default_charset = 'utf-8'
+  
+  # Work around interpolation deprecation problem: %d is replaced by
+  # {{count}}, even when we don't want them to.
+  # FIXME: We should probably fully convert to standard Rails I18n.
+  require 'i18n_interpolation_deprecation'
+  class I18n::Backend::Simple
+    def interpolate(locale, string, values = {})
+      interpolate_without_deprecated_syntax(locale, string, values)
+    end
+  end
+  
+  if RAILS_ENV != 'test'
+    begin
+      mail_settings = YAML.load(File.read("#{RAILS_ROOT}/config/mail.yml"))
+  
+      ActionMailer::Base.delivery_method = mail_settings['method']
+      ActionMailer::Base.server_settings = mail_settings['settings']
+    rescue
+      # Fall back to using sendmail by default
+      ActionMailer::Base.delivery_method = :sendmail
+    end
+  end
+end
